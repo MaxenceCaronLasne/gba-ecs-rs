5824bc5248ed69b4840567ba850b4824c1963e17
Maxence Caron-Lasne <maxence.caronlasne@gmail.com>
2025-09-07 18:52:29 +0200

    better API with proc macro

---
+++ ecs-derive/src/lib.rs
@@ -0,0 +1,138 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Error};
+
+#[proc_macro_derive(Component)]
+pub fn derive_component(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    match input.data {
+        syn::Data::Struct(_) => {
+            let expanded = quote! {
+                impl Component for #name {}
+            };
+
+            TokenStream::from(expanded)
+        }
+        _ => TokenStream::from(
+            Error::new(name.span(), "Component can only be derived for structs").to_compile_error(),
+        ),
+    }
+}
+
+#[proc_macro]
+pub fn define_world(input: TokenStream) -> TokenStream {
+    use syn::{braced, parse::Parse, parse::ParseStream, punctuated::Punctuated, Ident, Token};
+
+    struct WorldDefinition {
+        world_name: Ident,
+        components: Punctuated<Ident, Token![,]>,
+    }
+
+    impl Parse for WorldDefinition {
+        fn parse(input: ParseStream) -> syn::Result<Self> {
+            let world_name = input.parse()?;
+            let content;
+            braced!(content in input);
+            let components = content.parse_terminated(Ident::parse, Token![,])?;
+
+            Ok(WorldDefinition {
+                world_name,
+                components,
+            })
+        }
+    }
+
+    let world_def = parse_macro_input!(input as WorldDefinition);
+    let world_name = world_def.world_name;
+    let components = world_def.components;
+
+    let storage_fields = components.iter().map(|comp| {
+        let comp_lower = comp.to_string().to_lowercase();
+        let field_ident = quote::format_ident!("{}_storage", comp_lower);
+        quote! {
+            #field_ident: VecStorage<#comp>
+        }
+    });
+
+    let get_storage_impls = components.iter().map(|comp| {
+        let comp_lower = comp.to_string().to_lowercase();
+        let field_ident = quote::format_ident!("{}_storage", comp_lower);
+        quote! {
+            impl GetStorage<#comp> for #world_name {
+                type Storage = VecStorage<#comp>;
+
+                fn get_storage(&self) -> &Self::Storage {
+                    &self.#field_ident
+                }
+
+                fn get_storage_mut(&mut self) -> &mut Self::Storage {
+                    &mut self.#field_ident
+                }
+            }
+        }
+    });
+
+    let new_fields = components.iter().map(|comp| {
+        let comp_lower = comp.to_string().to_lowercase();
+        let field_ident = quote::format_ident!("{}_storage", comp_lower);
+        quote! {
+            #field_ident: VecStorage::new()
+        }
+    });
+
+    let expanded = quote! {
+        struct #world_name {
+            #(#storage_fields,)*
+            entity_count: usize,
+        }
+
+        #(#get_storage_impls)*
+
+        impl #world_name {
+            fn new() -> Self {
+                #world_name {
+                    #(#new_fields,)*
+                    entity_count: 0,
+                }
+            }
+
+            fn spawn_entity(&mut self) -> Entity {
+                let entity_id = self.entity_count;
+                self.entity_count += 1;
+                Entity{ index: entity_id }
+            }
+
+            fn add_component<C: Component>(&mut self, entity: Entity, component: C)
+            where
+                Self: GetStorage<C>,
+            {
+                self.get_storage_mut().insert(entity, component);
+            }
+
+            fn remove_component<C: Component>(&mut self, entity: Entity) -> Option<C>
+            where
+                Self: GetStorage<C>,
+            {
+                self.get_storage_mut().remove(entity)
+            }
+
+            fn get_component<C: Component>(&self, entity: Entity) -> Option<&C>
+            where
+                Self: GetStorage<C>,
+            {
+                self.get_storage().get(entity)
+            }
+
+            fn get_component_mut<C: Component>(&mut self, entity: Entity) -> Option<&mut C>
+            where
+                Self: GetStorage<C>,
+            {
+                self.get_storage_mut().get_mut(entity)
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
---
+++ ecs-rs/src/lib.rs
@@ -0,0 +1,131 @@
+#[derive(Clone, Copy, Debug)]
+struct Entity {
+    index: usize,
+}
+
+trait GetStorage<C: Component> {
+    type Storage: ComponentStorage<C>;
+    fn get_storage(&self) -> &Self::Storage;
+    fn get_storage_mut(&mut self) -> &mut Self::Storage;
+}
+
+trait Component: 'static {}
+
+trait ComponentStorage<C: Component> {
+    fn new() -> Self;
+    fn get(&self, entity: Entity) -> Option<&C>;
+    fn get_mut(&mut self, entity: Entity) -> Option<&mut C>;
+    fn insert(&mut self, entity: Entity, component: C);
+    fn remove(&mut self, entity: Entity) -> Option<C>;
+    fn ensure_capacity(&mut self, entity: Entity);
+}
+
+struct VecStorage<C: Component> {
+    components: Vec<Option<C>>,
+}
+
+impl<C: Component> ComponentStorage<C> for VecStorage<C> {
+    fn new() -> Self {
+        VecStorage {
+            components: Vec::new(),
+        }
+    }
+
+    fn get(&self, entity: Entity) -> Option<&C> {
+        self.components.get(entity.index)?.as_ref()
+    }
+
+    fn get_mut(&mut self, entity: Entity) -> Option<&mut C> {
+        self.components.get_mut(entity.index)?.as_mut()
+    }
+
+    fn insert(&mut self, entity: Entity, component: C) {
+        self.ensure_capacity(entity);
+        self.components[entity.index] = Some(component);
+    }
+
+    fn remove(&mut self, entity: Entity) -> Option<C> {
+        if entity.index < self.components.len() {
+            self.components[entity.index].take()
+        } else {
+            None
+        }
+    }
+
+    fn ensure_capacity(&mut self, entity: Entity) {
+        if entity.index >= self.components.len() {
+            self.components.resize_with(entity.index + 1, || None);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ecs_derive::{define_world, Component};
+
+    #[derive(Component)]
+    struct Position {
+        x: f32,
+        y: f32,
+    }
+
+    #[derive(Component)]
+    struct Velocity {
+        dx: f32,
+        dy: f32,
+    }
+
+    #[derive(Component)]
+    struct Health {
+        value: i32,
+    }
+
+    define_world!(World {
+        Position,
+        Velocity,
+        Health,
+    });
+
+    #[test]
+    fn it_works() {
+        let mut world = World::new();
+        let entity = world.spawn_entity();
+        world.add_component(entity, Position { x: 1.0, y: 0.0 });
+
+        let pos = world.get_component::<Position>(entity).unwrap();
+        assert_eq!(pos.x, 1.0);
+    }
+
+    #[test]
+    fn test_multiple_components() {
+        let mut world = World::new();
+        let entity = world.spawn_entity();
+
+        world.add_component(entity, Position { x: 1.0, y: 2.0 });
+        world.add_component(entity, Velocity { dx: 0.5, dy: -0.5 });
+        world.add_component(entity, Health { value: 100 });
+
+        let pos = world.get_component::<Position>(entity).unwrap();
+        let vel = world.get_component::<Velocity>(entity).unwrap();
+        let health = world.get_component::<Health>(entity).unwrap();
+
+        assert_eq!(pos.x, 1.0);
+        assert_eq!(pos.y, 2.0);
+        assert_eq!(vel.dx, 0.5);
+        assert_eq!(vel.dy, -0.5);
+        assert_eq!(health.value, 100);
+
+        {
+            let pos_mut = world.get_component_mut::<Position>(entity).unwrap();
+            pos_mut.x = 10.0;
+        }
+
+        let pos = world.get_component::<Position>(entity).unwrap();
+        assert_eq!(pos.x, 10.0);
+
+        let removed_health = world.remove_component::<Health>(entity);
+        assert_eq!(removed_health.unwrap().value, 100);
+        assert!(world.get_component::<Health>(entity).is_none());
+    }
+}
